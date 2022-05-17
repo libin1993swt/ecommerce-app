02bdbbbe21a6cc87a84d11f5be0538546ceffd43
Libin Abraham <libinabraham1993@gmail.com>
2022-05-17 23:59:27 +0530

    Menu active

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,5 +1,5 @@
 
 <div class="sidebar">
-  <a class="active" href="{{ route('products.index') }}">Products</a>
-  <a href="{{ route('orders.index') }}">Orders</a>
+  <a class="{{ (request()->segment(1) == 'products') ? 'active' : '' }}" href="{{ route('products.index') }}">Products</a>
+  <a class="{{ (request()->segment(1) == 'orders') ? 'active' : '' }}" href="{{ route('orders.index') }}">Orders</a>
 </div>
